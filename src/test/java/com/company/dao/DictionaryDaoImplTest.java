0687ce3699794288e04e54e80d026c3a6f6fd0f2
sergei1733 <sergei1733@yandex.ru>
2021-01-22 20:45:34 +0500

    test street, PassportOffice, area, registerOffice

--- src/test/java/com/company/dao/DictionaryDaoImplTest.java
+++ src/test/java/com/company/dao/DictionaryDaoImplTest.java
@@ -1,21 +1,21 @@
 package com.company.dao;
 
-import org.junit.Before;
+import com.company.studentOrder.domain.CountryArea;
+import com.company.studentOrder.domain.PassportOffice;
+import com.company.studentOrder.domain.RegisterOffice;
+import com.company.studentOrder.domain.Street;
+import com.company.studentOrder.exception.DaoException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.junit.Assert.*;
 
 public class DictionaryDaoImplTest {
 
@@ -38,15 +38,29 @@ public class DictionaryDaoImplTest {
     }
 
     @Test
-    public void testExample1(){
-        System.out.println("TEST1");
+    public void testStreet() throws DaoException {
+        List<Street> d = new DictionaryDaoImpl().findStreet("про");
+        Assert.assertTrue(d.size()==2);
     }
     @Test
-    public void testExample2(){
-        System.out.println("TEST2");
+    public void testPassportOffice() throws DaoException {
+        List<PassportOffice> po = new DictionaryDaoImpl().findPassportOffice("010020000000");
+        Assert.assertTrue(po.size()==2);
     }
     @Test
-    public void testExample3(){
-        System.out.println("TEST3");
+    public void testRegisterOffice() throws DaoException {
+        List<RegisterOffice> ro = new DictionaryDaoImpl().findregisterOffice("010010000000");
+        Assert.assertTrue(ro.size()==2);
+    }
+    @Test
+    public void testArea() throws DaoException {
+        List<CountryArea> ca1 = new DictionaryDaoImpl().findAreas("");
+        Assert.assertTrue(ca1.size()==2);
+        List<CountryArea> ca2 = new DictionaryDaoImpl().findAreas("020000000000");
+        Assert.assertTrue(ca2.size()==2);
+        List<CountryArea> ca3 = new DictionaryDaoImpl().findAreas("020010000000");
+        Assert.assertTrue(ca3.size()==2);
+        List<CountryArea> ca4 = new DictionaryDaoImpl().findAreas("020010010000");
+        Assert.assertTrue(ca4.size()==2);
     }
 }
---
+++ src/test/java/com/company/dao/StudentOrderDaoImplTest.java
@@ -0,0 +1,16 @@
+package com.company.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentOrderDaoImplTest {
+
+    @Test
+    public void saveStudentOrder() {
+    }
+
+    @Test
+    public void getStudentOrders() {
+    }
+}
